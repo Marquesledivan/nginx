c799f1dd0ae0bf3455c2376c1b559eb51ff64393
Ledivan Bernardo <ledivan_bernardo@yahoo.com.br>
2019-11-23 23:20:27 -0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,19 @@
+Learn more or give us feedback
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"log"
+)
+
+const webContent = "Hello World!"
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+	log.Fatal(http.ListenAndServe(":80", nil))
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, webContent)
+}
